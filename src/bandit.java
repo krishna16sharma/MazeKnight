858e6ad30d73f6fcda093436175194a1d97e7ae7
krishna16sharma <59202185+krishna16sharma@users.noreply.github.com>
2020-03-27 17:19:15 +0530

    Added more Enemies

---
+++ src/bandit.java
@@ -0,0 +1,24 @@
+public class bandit extends Enemies{
+    public bandit() throws InterruptedException{
+        this.setName("Bandit");
+        this.setHealth(50);
+        this.setDamage(15);
+    }
+
+    @Override
+    public void Encounter() throws InterruptedException {
+        Maze.delay(100);
+        System.out.println("Bandit:\n\t Give me everything you got!!");
+        Maze.delay(1000);
+        System.out.println("It\'s a bandit!");
+    }
+    public void SplAction(Enemies e,Knight k) throws InterruptedException{
+        Maze.delay(1000);
+        System.out.println("Bandit:\n\tYou won't get away!!");
+        Maze.delay(2000);
+        System.out.println("Bandit:\n\tTAKE THIS!!");
+        int dmg=(int)(20*20)/(20+k.armor);
+        k.health-=dmg;
+        System.out.println("You took "+dmg+" damage!");
+    }
+}
---
+++ src/banditChief.java
@@ -0,0 +1,24 @@
+public class banditChief extends Enemies {
+    public banditChief() throws InterruptedException{
+        this.setName("Bandit Chief");
+        this.setHealth(80);
+        this.setDamage(15);
+    }
+
+    @Override
+    public void Encounter() throws InterruptedException {
+        Maze.delay(100);
+        System.out.println("Bandit Chief:\n\t Then prepare to face the chief of bandits!!");
+        Maze.delay(1000);
+        System.out.println("It\'s the Bandit Chief!");
+    }
+    public void SplAction(Enemies e,Knight k) throws InterruptedException{
+        Maze.delay(1000);
+        System.out.println("Bandit Chief:\n\tI-I'll kill you!!");
+        Maze.delay(2000);
+        System.out.println("Bandit Chief:\n\tHAAAA!!");
+        int dmg=(int)(20*20)/(20+k.armor);
+        k.health-=dmg;
+        System.out.println("You took "+dmg+" damage!");
+    }
+}
